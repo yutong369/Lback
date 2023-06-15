38252d1b3c61c16c8ed7b33f75130f7333d29e76
lichang <2492282426@qq.com>
2023-06-16 01:55:40 +0800

    套餐管理lichang

--- src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import edu.czjt.reggie.entity.Setmeal;
 import org.apache.ibatis.annotations.Mapper;
 
+
+
 @Mapper
 public interface SetmealMapper extends BaseMapper<Setmeal> {
 }
