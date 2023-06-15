caf5af4800aa8b63470230071e7fe401279b7341
zxy342 <1808824686@qq.com>
2023-06-15 22:13:53 +0800

    zhangxinyue

---
+++ src/main/java/edu/czjt/reggie/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package edu.czjt.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.Employee;
+
+
+public interface EmployeeService extends IService<Employee> {
+}
