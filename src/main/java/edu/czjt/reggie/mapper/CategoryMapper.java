3f7f55549e2be943ae95abdbc7099a2dace682ff
Niuxy123 <1195543648@qq.com>
2023-06-16 08:59:34 +0800

    分类管理牛新月

---
+++ src/main/java/edu/czjt/reggie/mapper/CategoryMapper.java
@@ -0,0 +1,10 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CategoryMapper extends BaseMapper<Category> {
+}
